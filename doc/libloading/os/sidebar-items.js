8f49589f17d34b136e27ef52064fb96b6e260e89
kvark <kvark@fastmail.com>
2020-08-25 17:29:50 +0000

    Deploying to master from  @ 1935cfa28a5ca6f8f93abbec6e31cf13dba691e8 🚀

--- doc/libloading/os/sidebar-items.js
+++ doc/libloading/os/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["unix","UNIX implementation of dynamic library loading."],["windows","Windows implementation of dynamic library loading."]]});
+initSidebarItems({"mod":[["unix","UNIX implementation of dynamic library loading."]]});
