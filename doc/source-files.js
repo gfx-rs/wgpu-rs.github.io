dbe73766e3004b49a81f25265170e45cd31f40ff
cwfitzgerald <connorwadefitzgerald@gmail.com>
2022-05-15 19:22:10 +0000

    Deploying to master from  @ 202d81a64a85f4587d0074ad3d792ad87a625bff 🚀

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,4 +1,4 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["wgpu"] = {"name":"","dirs":[{"name":"backend","files":["direct.rs","mod.rs","native_gpu_future.rs"]},{"name":"util","files":["belt.rs","device.rs","encoder.rs","indirect.rs","init.rs","mod.rs"]}],"files":["lib.rs","macros.rs"]};
 sourcesIndex["wgpu_hal"] = {"name":"","dirs":[{"name":"auxil","files":["mod.rs","renderdoc.rs"]},{"name":"gles","files":["adapter.rs","command.rs","conv.rs","device.rs","egl.rs","mod.rs","queue.rs"]},{"name":"vulkan","files":["adapter.rs","command.rs","conv.rs","device.rs","instance.rs","mod.rs"]}],"files":["empty.rs","lib.rs"]};
 createSourceSidebar();
