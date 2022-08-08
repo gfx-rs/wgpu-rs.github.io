fb7d697bf2e0364200b35ae1b0b473dcbb34f4f8
cwfitzgerald <connorwadefitzgerald@gmail.com>
2022-08-08 04:50:12 +0000

    Deploying to master from  @ d655017439c10e8bd77ad80b353bb6b231c9d838 🚀

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,4 +1,5 @@
-var sourcesIndex = {};
-sourcesIndex["wgpu"] = {"name":"","dirs":[{"name":"backend","files":["direct.rs","mod.rs"]},{"name":"util","files":["belt.rs","device.rs","encoder.rs","indirect.rs","init.rs","mod.rs"]}],"files":["lib.rs","macros.rs"]};
-sourcesIndex["wgpu_hal"] = {"name":"","dirs":[{"name":"auxil","files":["mod.rs","renderdoc.rs"]},{"name":"gles","files":["adapter.rs","command.rs","conv.rs","device.rs","egl.rs","mod.rs","queue.rs"]},{"name":"vulkan","files":["adapter.rs","command.rs","conv.rs","device.rs","instance.rs","mod.rs"]}],"files":["empty.rs","lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"wgpu":["",[["backend",[],["direct.rs","mod.rs"]],["util",[],["belt.rs","device.rs","encoder.rs","indirect.rs","init.rs","mod.rs"]]],["lib.rs","macros.rs"]],\
+"wgpu_hal":["",[["auxil",[],["mod.rs","renderdoc.rs"]],["gles",[],["adapter.rs","command.rs","conv.rs","device.rs","egl.rs","mod.rs","queue.rs"]],["vulkan",[],["adapter.rs","command.rs","conv.rs","device.rs","instance.rs","mod.rs"]]],["empty.rs","lib.rs"]]\
+}');
 createSourceSidebar();
