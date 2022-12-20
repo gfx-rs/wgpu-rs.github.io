69417bb023b2e462a091e98e97ae4ba54ad0f586
cwfitzgerald <connorwadefitzgerald@gmail.com>
2022-12-20 00:21:06 +0000

    Deploying to master from  @ 052bd17d41c227628ba2a6b9c56d8f8af299fb8f 🚀

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = JSON.parse('{\
-"wgpu":["",[["backend",[],["direct.rs","mod.rs"]],["util",[],["belt.rs","device.rs","encoder.rs","indirect.rs","init.rs","mod.rs"]]],["lib.rs","macros.rs"]],\
+"wgpu":["",[["backend",[],["direct.rs","mod.rs"]],["util",[],["belt.rs","device.rs","encoder.rs","indirect.rs","init.rs","mod.rs"]]],["context.rs","lib.rs","macros.rs"]],\
 "wgpu_hal":["",[["auxil",[],["mod.rs","renderdoc.rs"]],["gles",[],["adapter.rs","command.rs","conv.rs","device.rs","egl.rs","mod.rs","queue.rs"]],["vulkan",[],["adapter.rs","command.rs","conv.rs","device.rs","instance.rs","mod.rs"]]],["empty.rs","lib.rs"]]\
 }');
 createSourceSidebar();
