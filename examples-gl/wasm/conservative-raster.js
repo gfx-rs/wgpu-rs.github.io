ca5ac245d1b6e6157b16095a7877c85c8b08aef4
cwfitzgerald <connorwadefitzgerald@gmail.com>
2023-07-19 23:05:28 +0000

    Deploying to master from @ gfx-rs/wgpu@a60efbde96dc17afae53cf4ebbd65544a82ca87b 🚀

--- examples-gl/wasm/conservative-raster.js
+++ examples-gl/wasm/conservative-raster.js
@@ -1898,8 +1898,8 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 57, __wbg_adapter_26);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5735 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1510, __wbg_adapter_46);
+    imports.wbg.__wbindgen_closure_wrapper5741 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1513, __wbg_adapter_46);
         return addHeapObject(ret);
     };
 
