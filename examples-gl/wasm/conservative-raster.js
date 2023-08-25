57e866f0e26bb8fd5640bdd88bfcf4850d46045f
nical <nical@fastmail.com>
2023-08-25 19:56:20 +0000

    Deploying to master from @ gfx-rs/wgpu@399637e2b8b8657a80913a36e5608bb01106c57b 🚀

--- examples-gl/wasm/conservative-raster.js
+++ examples-gl/wasm/conservative-raster.js
@@ -1917,8 +1917,8 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 177, __wbg_adapter_46);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5786 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1533, __wbg_adapter_49);
+    imports.wbg.__wbindgen_closure_wrapper5792 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1532, __wbg_adapter_49);
         return addHeapObject(ret);
     };
 
