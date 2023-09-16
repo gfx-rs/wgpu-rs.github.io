f1ed6e9fd35be90da1f42a4568f442c7b839a2fb
cwfitzgerald <connorwadefitzgerald@gmail.com>
2023-09-16 04:25:39 +0000

    Deploying to master from @ gfx-rs/wgpu@7c575a0b40b4cda2977d10ccc2c007ca2f77f3aa 🚀

--- examples-gl/wasm/hello-triangle.js
+++ examples-gl/wasm/hello-triangle.js
@@ -1591,8 +1591,8 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 156, __wbg_adapter_46);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper4095 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1517, __wbg_adapter_49);
+    imports.wbg.__wbindgen_closure_wrapper4098 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1519, __wbg_adapter_49);
         return addHeapObject(ret);
     };
 
