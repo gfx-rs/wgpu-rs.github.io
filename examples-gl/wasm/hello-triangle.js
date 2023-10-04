2daecba2ab92926f3ce3a76d8eb30be7b26d330e
nical <nical@fastmail.com>
2023-10-04 09:18:56 +0000

    Deploying to master from @ gfx-rs/wgpu@32b761a591b677013fdb20aafd3dab64e4604f5f 🚀

--- examples-gl/wasm/hello-triangle.js
+++ examples-gl/wasm/hello-triangle.js
@@ -1591,8 +1591,8 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 156, __wbg_adapter_46);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper4076 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1538, __wbg_adapter_49);
+    imports.wbg.__wbindgen_closure_wrapper4077 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1536, __wbg_adapter_49);
         return addHeapObject(ret);
     };
 
