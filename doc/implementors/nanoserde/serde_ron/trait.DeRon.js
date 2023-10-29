619d7162cf2d977281fcbaafc128798bdadd8986
Wumpf <r_andreas2@web.de>
2023-10-29 16:36:27 +0000

    Deploying to master from @ gfx-rs/wgpu@83c3fbc55c6a6be6d4e90c301a962095d005ac41 🚀

--- doc/implementors/nanoserde/serde_ron/trait.DeRon.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"hlsl_snapshots":[["impl DeRon for <a class=\"struct\" href=\"hlsl_snapshots/struct.Config.html\" title=\"struct hlsl_snapshots::Config\">Config</a>"],["impl DeRon for <a class=\"struct\" href=\"hlsl_snapshots/struct.ConfigItem.html\" title=\"struct hlsl_snapshots::ConfigItem\">ConfigItem</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
