2cb3100737d0706e5dc7a4bf57640bd47c8f00ee
cwfitzgerald <connorwadefitzgerald@gmail.com>
2023-11-21 04:58:14 +0000

    Deploying to master from @ gfx-rs/wgpu@4b10ce7e5bb42d966800c46e28105eddc4b3e8fd 🚀

--- doc/trait.impl/nanoserde/serde_ron/trait.SerRon.js
+++ doc/trait.impl/nanoserde/serde_ron/trait.SerRon.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hlsl_snapshots":[["impl SerRon for <a class=\"struct\" href=\"hlsl_snapshots/struct.ConfigItem.html\" title=\"struct hlsl_snapshots::ConfigItem\">ConfigItem</a>"],["impl SerRon for <a class=\"struct\" href=\"hlsl_snapshots/struct.Config.html\" title=\"struct hlsl_snapshots::Config\">Config</a>"]]
+"hlsl_snapshots":[["impl SerRon for <a class=\"struct\" href=\"hlsl_snapshots/struct.Config.html\" title=\"struct hlsl_snapshots::Config\">Config</a>"],["impl SerRon for <a class=\"struct\" href=\"hlsl_snapshots/struct.ConfigItem.html\" title=\"struct hlsl_snapshots::ConfigItem\">ConfigItem</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
