aa4c5c69419641c791841c968eb9da8304351a9a
cwfitzgerald <connorwadefitzgerald@gmail.com>
2024-01-17 20:55:59 +0000

    Deploying to master from @ gfx-rs/wgpu@3716b8a1bc284bb543fa40f76df76e58bfabadc1 🚀

--- doc/trait.impl/wgpu_core/identity/trait.IdentityHandlerFactory.js
+++ doc/trait.impl/wgpu_core/identity/trait.IdentityHandlerFactory.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"player":[["impl&lt;I: <a class=\"trait\" href=\"wgpu_core/id/trait.TypedId.html\" title=\"trait wgpu_core::id::TypedId\">TypedId</a>&gt; <a class=\"trait\" href=\"wgpu_core/identity/trait.IdentityHandlerFactory.html\" title=\"trait wgpu_core::identity::IdentityHandlerFactory\">IdentityHandlerFactory</a>&lt;I&gt; for <a class=\"struct\" href=\"player/struct.IdentityPassThroughFactory.html\" title=\"struct player::IdentityPassThroughFactory\">IdentityPassThroughFactory</a>"]],
+"player":[["impl&lt;I: TypedId&gt; IdentityHandlerFactory&lt;I&gt; for <a class=\"struct\" href=\"player/struct.IdentityPassThroughFactory.html\" title=\"struct player::IdentityPassThroughFactory\">IdentityPassThroughFactory</a>"]],
 "wgpu":[],
 "wgpu_core":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
