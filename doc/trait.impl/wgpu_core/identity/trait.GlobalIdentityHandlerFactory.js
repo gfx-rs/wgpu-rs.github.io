4e6b77b1e334644af608a5d046f2e1364ad60cf5
cwfitzgerald <connorwadefitzgerald@gmail.com>
2024-01-17 20:25:40 +0000

    Deploying to master from @ gfx-rs/wgpu@484457d95993b00b91905fae0e539a093423cc28 🚀

--- doc/trait.impl/wgpu_core/identity/trait.GlobalIdentityHandlerFactory.js
+++ doc/trait.impl/wgpu_core/identity/trait.GlobalIdentityHandlerFactory.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"player":[["impl GlobalIdentityHandlerFactory for <a class=\"struct\" href=\"player/struct.IdentityPassThroughFactory.html\" title=\"struct player::IdentityPassThroughFactory\">IdentityPassThroughFactory</a>"]],
+"player":[["impl <a class=\"trait\" href=\"wgpu_core/identity/trait.GlobalIdentityHandlerFactory.html\" title=\"trait wgpu_core::identity::GlobalIdentityHandlerFactory\">GlobalIdentityHandlerFactory</a> for <a class=\"struct\" href=\"player/struct.IdentityPassThroughFactory.html\" title=\"struct player::IdentityPassThroughFactory\">IdentityPassThroughFactory</a>"]],
 "wgpu":[],
 "wgpu_core":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
