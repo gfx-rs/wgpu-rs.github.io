0b177d5ab4bcf8750e9c7607e82123da6cdd2728
cwfitzgerald <connorwadefitzgerald@gmail.com>
2024-01-21 23:17:02 +0000

    Deploying to master from @ gfx-rs/wgpu@e5c62fb5bd29c5bd99f2445e3b089689c0e145e2 🚀

--- examples/webgl2.js
+++ examples/webgl2.js
@@ -2493,11 +2493,11 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 723, __wbg_adapter_56);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper13035 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1380, __wbg_adapter_59);
+    imports.wbg.__wbindgen_closure_wrapper13842 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1056, __wbg_adapter_59);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper21693 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper21798 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1789, __wbg_adapter_62);
         return addHeapObject(ret);
     };
