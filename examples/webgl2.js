8b817bbfb01830559638b81b5f17718e58e1eb0e
nical <nical@fastmail.com>
2024-01-31 07:18:36 +0000

    Deploying to master from @ gfx-rs/wgpu@0888a630a0ed6400a955e12646cfa7994a6f41d6 🚀

--- examples/webgl2.js
+++ examples/webgl2.js
@@ -2501,7 +2501,7 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 747, __wbg_adapter_59);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper18071 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper18070 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1656, __wbg_adapter_62);
         return addHeapObject(ret);
     };
