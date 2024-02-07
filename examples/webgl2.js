ca45646b0fb54b1e1e86cc0f576932d2968574ed
ErichDonGubler <erichdongubler@gmail.com>
2024-02-07 16:19:58 +0000

    Deploying to master from @ gfx-rs/wgpu@499bd64c292b2cd436b8c6aceafcfac0268f9476 🚀

--- examples/webgl2.js
+++ examples/webgl2.js
@@ -2497,12 +2497,12 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 723, __wbg_adapter_56);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper16994 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 745, __wbg_adapter_59);
+    imports.wbg.__wbindgen_closure_wrapper16492 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1072, __wbg_adapter_59);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper18091 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1660, __wbg_adapter_62);
+    imports.wbg.__wbindgen_closure_wrapper18105 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1661, __wbg_adapter_62);
         return addHeapObject(ret);
     };
 
