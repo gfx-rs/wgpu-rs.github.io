0f6a51223c3456de7a54fd9a6676cafc03f60a47
teoxoy <28601907+teoxoy@users.noreply.github.com>
2024-06-03 09:34:09 +0000

    Deploying to master from @ gfx-rs/wgpu@badcaee6e33812df6023d2322ee55d6a9f992d1c 🚀

--- examples/webgl2.js
+++ examples/webgl2.js
@@ -2561,8 +2561,8 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 778, __wbg_adapter_56);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper16789 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1140, __wbg_adapter_59);
+    imports.wbg.__wbindgen_closure_wrapper15609 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 836, __wbg_adapter_59);
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper20826 = function(arg0, arg1, arg2) {
