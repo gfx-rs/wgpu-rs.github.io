cea3dfcaedad1756ded61b19f48f9f58436d642c
teoxoy <28601907+teoxoy@users.noreply.github.com>
2024-09-13 15:31:57 +0000

    Deploying to master from @ gfx-rs/wgpu@c8beade1877251c494036fc3661b04ec6aad63a9 🚀

--- doc/wgpu_core/instance/sidebar-items.js
+++ doc/wgpu_core/instance/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AdapterInputs","CreateSurfaceError","GetSurfaceSupportError","RequestAdapterError","RequestDeviceError"],"fn":["parse_backends_from_comma_list"],"struct":["Adapter","FailedLimit","Instance","Surface"],"type":["RequestAdapterOptions"]};
+window.SIDEBAR_ITEMS = {"enum":["CreateSurfaceError","GetSurfaceSupportError","RequestAdapterError","RequestDeviceError"],"fn":["parse_backends_from_comma_list"],"struct":["Adapter","FailedLimit","Instance","Surface"],"type":["RequestAdapterOptions"]};
