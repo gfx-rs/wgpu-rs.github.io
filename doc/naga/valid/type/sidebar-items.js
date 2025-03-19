04b64659f19888c10a25383a5201ade49fdcb20a
ErichDonGubler <erichdongubler@gmail.com>
2025-03-19 16:27:46 +0000

    Deploying to master from @ gfx-rs/wgpu@c6286791febc64cf8ef054b5356c2669327ef51c 🚀

--- doc/naga/valid/type/sidebar-items.js
+++ doc/naga/valid/type/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Disalignment","TypeError","WidthError"],"fn":["check_member_layout","ptr_space_argument_flag"],"struct":["TypeFlags","TypeInfo"],"type":["LayoutCompatibility"]};
+window.SIDEBAR_ITEMS = {"enum":["Disalignment","PushConstantError","TypeError","WidthError"],"fn":["check_member_layout","ptr_space_argument_flag"],"struct":["TypeFlags","TypeInfo"],"type":["LayoutCompatibility","PushConstantCompatibility"]};
