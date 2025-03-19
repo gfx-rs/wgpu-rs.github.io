70515bf95f405de923d946cf15b52468dc4b47a6
ErichDonGubler <erichdongubler@gmail.com>
2025-03-19 14:03:12 +0000

    Deploying to master from @ gfx-rs/wgpu@b85a451dc999b8b23d4f52fb6a0906d5e7d45ac0 🚀

--- doc/naga/front/wgsl/sidebar-items.js
+++ doc/naga/front/wgsl/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ImplementedLanguageExtension","LanguageExtension","UnimplementedLanguageExtension"],"fn":["parse_str"],"mod":["error","index","lower","parse","to_wgsl"],"struct":["Frontend","ParseError"]};
+window.SIDEBAR_ITEMS = {"enum":["ImplementedLanguageExtension","LanguageExtension","UnimplementedLanguageExtension"],"fn":["parse_str"],"mod":["error","index","lower","parse","to_wgsl"],"struct":["Frontend","ParseError"],"type":["Result"]};
