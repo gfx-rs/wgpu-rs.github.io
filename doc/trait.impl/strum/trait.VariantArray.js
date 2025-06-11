a7a9d4190afd91a20cf8da9c37e1544a495ef592
ErichDonGubler <erichdongubler@gmail.com>
2025-06-11 08:33:06 +0000

    Deploying to master from @ gfx-rs/wgpu@630905134b75148677cc9541257080bc1c349419 🚀

--- doc/trait.impl/strum/trait.VariantArray.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["naga",[["impl VariantArray for <a class=\"enum\" href=\"naga/front/wgsl/parse/directive/language_extension/enum.ImplementedLanguageExtension.html\" title=\"enum naga::front::wgsl::parse::directive::language_extension::ImplementedLanguageExtension\">ImplementedLanguageExtension</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[287]}
