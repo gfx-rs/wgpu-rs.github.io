0a2ba5cf317e1e84a51a71c202500bcf11f66a77
jimblandy <jimb@red-bean.com>
2025-07-10 16:54:26 +0000

    Deploying to master from @ gfx-rs/wgpu@dc924bc715e64662deb7c7062789a843abc58c65 🚀

--- doc/type.impl/std/primitive.u64.js
+++ doc/type.impl/std/primitive.u64.js
@@ -1,9 +1,9 @@
 (function() {
-    var type_impls = Object.fromEntries([["wgpu",[]],["wgpu_hal",[]],["wgpu_types",[]]]);
+    var type_impls = Object.fromEntries([["wgpu",[]],["wgpu_hal",[["<details class=\"toggle implementors-toggle\" open><summary><section id=\"impl-ShouldBeNonZeroExt-for-u64\" class=\"impl\"><a class=\"src rightside\" href=\"src/wgpu_hal/lib.rs.html#2069-2073\">Source</a><a href=\"#impl-ShouldBeNonZeroExt-for-u64\" class=\"anchor\">§</a><h3 class=\"code-header\">impl <a class=\"trait\" href=\"wgpu_hal/trait.ShouldBeNonZeroExt.html\" title=\"trait wgpu_hal::ShouldBeNonZeroExt\">ShouldBeNonZeroExt</a> for <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.1/std/primitive.u64.html\">u64</a></h3></section></summary><div class=\"impl-items\"><section id=\"method.get\" class=\"method trait-impl\"><a class=\"src rightside\" href=\"src/wgpu_hal/lib.rs.html#2070-2072\">Source</a><a href=\"#method.get\" class=\"anchor\">§</a><h4 class=\"code-header\">fn <a href=\"wgpu_hal/trait.ShouldBeNonZeroExt.html#tymethod.get\" class=\"fn\">get</a>(&amp;self) -&gt; <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.1/std/primitive.u64.html\">u64</a></h4></section></div></details>","ShouldBeNonZeroExt","wgpu_hal::FenceValue"]]],["wgpu_types",[]]]);
     if (window.register_type_impls) {
         window.register_type_impls(type_impls);
     } else {
         window.pending_type_impls = type_impls;
     }
 })()
-//{"start":55,"fragment_lengths":[11,16,18]}
+//{"start":55,"fragment_lengths":[11,1092,18]}
