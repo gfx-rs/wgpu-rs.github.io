8844af7b1ad551f0b5c98409197d9a2ef532d02b
teoxoy <28601907+teoxoy@users.noreply.github.com>
2025-07-21 11:52:53 +0000

    Deploying to master from @ gfx-rs/wgpu@2c81896e85620c4dcfd175eac9b088e25a0776cc 🚀

---
+++ doc/trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["wgpu_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"enum\" href=\"wgpu_core/id/enum.SerialId.html\" title=\"enum wgpu_core::id::SerialId\">SerialId</a>&gt; for <a class=\"struct\" href=\"wgpu_core/id/struct.RawId.html\" title=\"struct wgpu_core::id::RawId\">RawId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[398]}
