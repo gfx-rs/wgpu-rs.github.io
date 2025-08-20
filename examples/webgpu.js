e4c669bd8fd1993edabacf890b47a733f8673507
andyleiserson <aleiserson@mozilla.com>
2025-08-20 22:01:22 +0000

    Deploying to master from @ gfx-rs/wgpu@e7a99be0584fc9a8517c3e22f34eb732b7524920 🚀

--- examples/webgpu.js
+++ examples/webgpu.js
@@ -267,7 +267,7 @@ function __wbg_adapter_54(arg0, arg1, arg2) {
 }
 
 function __wbg_adapter_57(arg0, arg1, arg2) {
-    wasm.closure906_externref_shim(arg0, arg1, arg2);
+    wasm.closure908_externref_shim(arg0, arg1, arg2);
 }
 
 function __wbg_adapter_60(arg0, arg1, arg2) {
@@ -2178,11 +2178,11 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper11674 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 907, __wbg_adapter_57);
+    imports.wbg.__wbindgen_closure_wrapper10557 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 909, __wbg_adapter_57);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper13247 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper13246 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1019, __wbg_adapter_60);
         return ret;
     };
