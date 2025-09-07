633051df5cfa206d1c39778b906a053eb3725405
Wumpf <r_andreas2@web.de>
2025-09-07 07:36:22 +0000

    Deploying to master from @ gfx-rs/wgpu@832609959d09030534adbba9c8868e76dfb31277 🚀

--- doc/trait.impl/core/convert/trait.AsMut.js
+++ doc/trait.impl/core/convert/trait.AsMut.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["wgpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"wgpu/struct.BufferViewMut.html\" title=\"struct wgpu::BufferViewMut\">BufferViewMut</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"wgpu/struct.QueueWriteBufferView.html\" title=\"struct wgpu::QueueWriteBufferView\">QueueWriteBufferView</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["wgpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"wgpu/struct.BufferViewMut.html\" title=\"struct wgpu::BufferViewMut\">BufferViewMut</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"wgpu/struct.QueueWriteBufferView.html\" title=\"struct wgpu::QueueWriteBufferView\">QueueWriteBufferView</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[779]}
+//{"start":57,"fragment_lengths":[769]}
