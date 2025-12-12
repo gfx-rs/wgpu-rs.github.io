3cdc1210bf4a92354eb3ef3443acc22d7996763c
cwfitzgerald <connorwadefitzgerald@gmail.com>
2025-12-12 19:12:14 +0000

    Deploying to master from @ gfx-rs/wgpu@ea361fc11422af69a460a89ea006642174ccb251 🚀

--- doc/trait.impl/gpu_alloc_types/device/trait.MemoryDevice.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["wgpu_hal",[["impl MemoryDevice&lt;DeviceMemory&gt; for <a class=\"struct\" href=\"wgpu_hal/vulkan/struct.DeviceShared.html\" title=\"struct wgpu_hal::vulkan::DeviceShared\">DeviceShared</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[195]}
