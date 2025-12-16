551354897e0ca20d9d7ae0f5059069290d6a1a23
teoxoy <28601907+teoxoy@users.noreply.github.com>
2025-12-16 12:14:11 +0000

    Deploying to master from @ gfx-rs/wgpu@e52e8336b061d2cdcc4f932ea03e36f0f5f46a79 🚀

--- doc/wgpu_core/command/bind/sidebar-items.js
+++ doc/wgpu_core/command/bind/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["BinderError"],"fn":["compute_nonoverlapping_ranges"],"mod":["compat"],"struct":["Binder","EntryPayload","ImmediateChange","LateBufferBinding"]};
+window.SIDEBAR_ITEMS = {"enum":["BinderError"],"mod":["compat"],"struct":["Binder","EntryPayload","LateBufferBinding"]};
