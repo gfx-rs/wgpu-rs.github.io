1d1d1c5b896f0c006df964973b721f53f6eb75da
jimblandy <jimb@red-bean.com>
2026-03-04 16:56:11 +0000

    Deploying to master from @ gfx-rs/wgpu@d152e7369b31a8a80f03b1e58567503779d431e7 🚀

--- examples/webgpu.js
+++ examples/webgpu.js
@@ -1922,7 +1922,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000003: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { dtor_idx: 1692, function: Function { arguments: [Externref], shim_idx: 3581, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { dtor_idx: 1692, function: Function { arguments: [Externref], shim_idx: 3535, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__hd8358f28af5be9eb, wasm_bindgen__convert__closures_____invoke__h9f53e6ead521a5f8);
             return ret;
         },
