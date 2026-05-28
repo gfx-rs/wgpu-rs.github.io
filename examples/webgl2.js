20de83c68ac71fdfc2b99a47ec8eb17d4974673e
andyleiserson <aleiserson@mozilla.com>
2026-05-28 21:55:04 +0000

    Deploying to master from @ gfx-rs/wgpu@a1fceed0710fafed9abe0f7fb16c0a19bb390bf2 🚀

--- examples/webgl2.js
+++ examples/webgl2.js
@@ -1618,7 +1618,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 3847, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 3849, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__h6ca030526cd61a5e);
             return ret;
         },
